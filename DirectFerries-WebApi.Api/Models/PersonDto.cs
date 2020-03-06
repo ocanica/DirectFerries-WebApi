11e5812f9cfd836e034bd7b192d3b2a983ac266b
Lloyd Headbush <ocanica@gmail.com>
2020-03-06 21:12:48 +0000

    Initial commit

---
+++ DirectFerries-WebApi.Api/Models/PersonDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectFerries_WebApi.Api.Models
+{
+    public class PersonDto
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Score { get; set; }
+    }
+}
